3e470a1d6006ef9eb09252c1a2b56996a7d76267
deanrad <chicagogrooves@gmail.com>
2022-04-06 22:38:38 -0500

    got css, TS working

---
+++ components/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function App() {
+  // return React.createElement('h2', {}, 'Hello');
+  return <h2>Hello jsx</h2>;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import './App.css'
+import './index.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
