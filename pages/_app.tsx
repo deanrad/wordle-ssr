47ffe8828f588808ffee6a652e90e6ed625b254a
deanrad <chicagogrooves@gmail.com>
2022-04-06 22:51:07 -0500

    broke it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import './App.css';
-import './index.css';
+// import './App.css';
+// import './index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
